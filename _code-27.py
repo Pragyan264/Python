e9fad95b703f1226c26839215482c9d4f048881e
Pragyan Kasaudhan <pragyankasaudhan735@gmail.com>
2026-07-03 17:39:23 +0530

    Remove unnecessary blank lines in recursive sum of digits program

--- _code-27.py
+++ _code-27.py
@@ -1,13 +1,9 @@
 # Write a program to Recursive sum of digits.
 def sum_digit(num):
-   
-    
-    
     if(num==0):
         return 0
     else:
         return num%10+(sum_digit(num//10))
-    
 num=int(input("Enter the number:"))
 digit=sum_digit(num)
 print("The sum of digit is",digit)
