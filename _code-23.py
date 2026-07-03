fbfb26d3d36d13a3961194b266e0b6c6cbf87784
Pragyan Kasaudhan <pragyankasaudhan735@gmail.com>
2026-07-03 16:07:14 +0530

    Add program to count set bits in a number

---
+++ _code-23.py
@@ -0,0 +1,9 @@
+# Write a program to Count set bits in a number
+num=int(input("Enter the number:"))
+b=bin(num)
+b=b.replace("0b","")
+count=0
+for i in range(0,len(b)):
+    if(b[i]=="1"):
+        count+=1
+print(count)
