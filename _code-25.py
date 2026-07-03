19ef44a908f2c927572a613c2a597c809d4344f8
Pragyan Kasaudhan <pragyankasaudhan735@gmail.com>
2026-07-03 17:01:46 +0530

    Refactor factorial function to use recursion correctly and update output message

--- _code-25.py
+++ _code-25.py
@@ -1,11 +1,9 @@
 # Write a program to Recursive factorial.
-def Fact(num):
-    if(num==1):
-        return 0
-    elif(num==2):
+def fact(n):
+    if(n==0 or n==1):
         return 1
     else:
-        return Fact(num-1)+Fact(num-2)
+        return n*fact(n-1)
 num=int(input("Enter the number:"))
-fact=Fact(num)
-print("The Factorial at",num,"th number is",fact)
+Fact=fact(num)
+print("The Factorial of number is",Fact)
