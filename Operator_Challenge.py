690955af0dd66c3b10435b10c01b4bca7eab2f17
Pragyan Kasaudhan <pragyankasaudhan735@gmail.com>
2026-07-05 16:21:22 +0530

    Add program to calculate and display the square and cube of a number

---
+++ Operator_Challenge.py
@@ -0,0 +1,7 @@
+# Operator Challenge
+# Write a program that:
+# Takes an integer as input from the user.
+# Prints the square and cube of that number.
+num=int(input("Enteer the number:"))
+print("The square of a number is",num**2)
+print("The cube of a number is",num**3)
