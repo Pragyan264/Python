c6cd3718bd45cada43d34e7fc63b9f806b7d463f
Pragyan Kasaudhan <pragyankasaudhan735@gmail.com>
2026-07-05 16:19:46 +0530

    Add program to determine if a number is positive, negative, or zero

---
+++ number.py
@@ -0,0 +1,11 @@
+# Write a program that asks the user for a number and prints whether it is
+# positive, negative, or zero.
+num=int(input("Enter the number:"))
+if(num>0):
+    print("The number is Positive.")
+elif(num<0):
+    print("The number is Negative.")
+elif(num==0):
+    print("The number is Zero")
+else:
+    print("Something went wrong!")
