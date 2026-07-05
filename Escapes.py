910c03f0417d0959a2a96920f6f2e5a3304da4c8
Pragyan Kasaudhan <pragyankasaudhan735@gmail.com>
2026-07-05 16:18:56 +0530

    Add program to demonstrate escape sequences in string output

---
+++ Escapes.py
@@ -0,0 +1,5 @@
+# Print the following output using escape sequences:
+# Harry said, "Python is awesome!"
+# This is on a new line.
+# This is a tab -> <- here
+print('Harry said,"Python is Awesome!"\nThis is new line.\nThis is tab ->\t<- here')
