de2f04b29b3a503192bdf7c6bb6a48a7a03f9d56
Pragyan Kasaudhan <pragyankasaudhan735@gmail.com>
2026-07-09 19:39:31 +0530

    Add program to find the sum of two numbers

---
+++ _code-41.py
@@ -0,0 +1,7 @@
+# Write a program to Write function to find sum of two numbers.
+def sum(x,y):
+    return x+y
+x=int(input("Enter the First number:"))
+y=int(input("Enter the Second number:"))
+S=sum(x,y)
+print("The sum of two number is",S)
